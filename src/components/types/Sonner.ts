cf5ae9ffa95e459e33d3dc5cdf4897c7cc5e4f68
Devin Xie <devinxie418@gmail.com>
2026-06-14 21:11:01 -0400

    feat: sonner

---
+++ src/components/types/Sonner.ts
@@ -0,0 +1,5 @@
+export type Sonner = {
+    type: "success" | "error" | "warning" | undefined
+    title?: string,
+    description?: string
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,22 @@ import { BrowserRouter } from "react-router";
 import './index.css'
 import App from './App.tsx'
 
+// components
+import { Toaster } from 'sonner';
+
+// contexts
+import { UIProvider } from '@/components/contexts/UIContext.tsx';
+import { AuthProvider } from '@/components/contexts/AuthContext.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <UIProvider>
+        <Toaster />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UIProvider>
+    </AuthProvider>
   </StrictMode>,
 )
